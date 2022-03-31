7a5babf07052a8dd097f0e6defa983b58decb93a
DECODER69 <84758030+DECODER69@users.noreply.github.com>
2022-03-31 15:10:57 +0800

    ssss

--- NstpSystem/settings.py
+++ NstpSystem/settings.py
@@ -142,7 +142,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATICFILES_DIRS=[(os.path.join(BASE_DIR,'static'))]
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
-django_heroku.settings(locals())
+
 
 
 # Default primary key field type
